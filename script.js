fef082fcd13c560213ec9f15e92732fb3d3c140e
Andrew Carroll <andrewc411@yahoo.com>
2023-07-14 15:36:26 -0400

    adding javascript functionality

--- script.js
+++ script.js
@@ -1,94 +1,134 @@
-// const musicContainer = document.querySelector('.music-container');
-// const playBtn = document.querySelector('#play');
-// const prevBtn = document.querySelector('#prev');
-// const nextBtn = document.querySelector('#next');
-// const audio = document.querySelector('#audio');
-// const progress = document.querySelector('.progress');
-// const progressContainer = document.querySelector('.progress-container');
-// const title = document.querySelector('#title');
-// const cover = document.querySelector('#cover');
+let now_playing = document.querySelector('.now-playing');
+let track_art = document.querySelector('.track-art');
+let track_name = document.querySelector('.track-name');
+let track_artist = document.querySelector('.track-artist');
 
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
 
-// // console.log(playBtn);
+let seek_slider = document.querySelector('.seek-slider');
+let volume_slider = document.querySelector('.volume-slider');
+let current_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+let wave = document.getElementById('wave');
+let randomIcon = document.querySelector('.fa-random');
+let current_track = document.createElement('audio');
 
-// // Song titles
-// // Fill array with names of songs
-// const songs = ['Sunshine', 'Crystalised', 'Im Leaving', 'The Promised Land']
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
 
-// //Keep track of the songs
-// let songIndex = 1;
+const music_list = [
+  {
+    img: 'img/Atmosphere-Sunshine.jpg',
+    name: 'Sunshine',
+    artist: 'Atmosphere',
+    music: 'music/Atmosphere-Sunshine.mp3'
+  },
+  {
+    img: 'img/Crystalised.jpg',
+    name: 'Crystalised',
+    artist: 'Gorillaz',
+    music: 'music/Gorillaz - Crystalised.mp3'
+  },
+  {
+    img: 'img/Im Leaving.jpg',
+    name: "I'm Leaving",
+    artist: 'Mos Def',
+    music: "music/Mos Def - I'm Leaving.mp3"
+  },
+  {
+    img: 'img/The Promised Land.jpg',
+    name: 'The Promised Land',
+    artist: 'J. Cole ft. Andre 3000',
+    music: 'music/The Promised Land ft. J. Cole Andre 3000.mp3'
+  }
+];
 
-// // Initially load song into DOM
-// loadSong(songs[songIndex])
+loadTrack(track_index);
 
-// //Update song details
-// function loadSong(song) {
-//     title.innerText = song
-//     audio.src = `music/${song}.mp3`
-//     cover.src = `img/${song}.jpg`
-// }
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  reset();
 
-// // Retrieve the 'play' button element using the updated ID
-// // const playBtn = document.getElementById('#play');
+  current_track.src = music_list[track_index].music;
+  current_track.load();
 
-// // Check if the element was found before modifying it
-// if (playBtn) {
-//   // Use querySelector() on playBtn to select the nested 'i.fas' element
-//   const playBtn = document.getElementById('#play');
-//   const playIcon = playBtn.querySelector('i.fas');
-  
-//   // Modify the classList properties of the 'i.fas' element as intended
-//   playIcon.classList.remove('fa-play');
-//   playIcon.classList.add('fa-pause');
-// } else {
-//   console.log('Could not find play button element.');
-// }
+  track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+  track_name.textContent = music_list[track_index].name;
+  track_artist.textContent = music_list[track_index].artist;
+  now_playing.textContent = "Playing track " + (track_index + 1) + " of " + music_list.length;
 
+  updateTimer = setInterval(setUpdate, 1000);
 
-// function playSong() {
-// // check if the element exists
-// if (playBtn) {
-//   // toggle between fa-play and fa-pause classes
-//   musicContainer.classList.add('play');
-//   playIcon.classList.remove('fa-pause');
-//   playIcon.classList.add('fa-play');
-// } else {
-//   console.log('Element not found.');
-// }
-// }
+  current_track.addEventListener('ended', nextTrack);
+  random_bg_color();
+}
 
-// function pauseSong() {
-// // check if the element exists
-// if (playBtn) {
-//   const playIcon = playBtn.querySelector('i-fas');
-//     // toggle between fa-play and fa-pause classes
-//     musicContainer.classList.remove('play')
-//     playIcon.classList.remove('fa-pause');
-//     playIcon.classList.add('fa-play');
-//   } else {
-//     console.log('Element not found.');
-//   }
-// }
-// // function playSong() {
-// //     musicContainer.classList.add('play')
-// //     playBtn.querySelector('i.fas').classList.remove('fa-play')
-// //     playBtn.querySelector('i.fas').classList.add('fa-pause')
-// // }
+function random_bg_color() {
+  let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+  let a;
 
-// // function pauseSong() {
-// //     musicContainer.classList.remove('play')
-// //     playBtn.querySelector('i.fas').classList.remove('fa-pause')
-// //     playBtn.querySelector('i.fas').classList.add('fa-play')
-// // }
+  function populate(a){
+    for(let i = 0; i < 6; i++){
+      let x = Math.round(Math.random() * 14);
+      let y = hex[x];
+      a += y;
+    }
+    return a;
+  }
+  let Color1 = populate('#');
+  let Color2 = populate('#');
+  var angle = 'to right';
 
-// // Event Listeners
-// playBtn.addEventListener("click", () => {
-//     const isPlaying = musicContainer.classList.contains('play')
+  let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+  document.body.style.background = gradient;
+}
 
-//     if(isPlaying) {
-//         pauseSong();
-//     }
-//     else {
-//         playSong();
-//     }
-// })
+function reset() {
+  current_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+function randomTrack() {
+  isRandom ? pauseRandom() : playRandom();
+}
+function playRandom() {
+  isRandom = true;
+  randomIcon.classList.add("randomActive");
+}
+function pauseRandom() {
+  isRandom = false;
+  randomIcon.classList.remove("randomActive");
+}
+function repeatTrack() {
+  let current_index = track_index;
+  loadTrack(current_index);
+  playTrack();
+}
+function playpauseTrack() {
+  isPlaying ? pauseTrack() : playTrack();
+}
+function playTrack() {
+  current_track.play();
+  isPlaying = true;
+  wave.classList.add('loader');
+  track_art.classList.add('rotate');
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+function pauseTrack() {
+  current_track.pause();
+  isPlaying = false;
+  wave.classList.remove('loader');
+  track_art.classList.remove('rotate');
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+
+function nextTrack() {
+  if(track_index < music_list.length - 1 && isRandom === false) {
+    track_index += 1;
+  }
+  else if(track_index < music_list.length - 1 && isRandom === true) {
+}
