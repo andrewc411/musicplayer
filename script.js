ce60ad6936d6d71cd1842fd0cc0e1fae771e9072
Andrew Carroll <andrewc411@yahoo.com>
2023-07-15 14:26:32 -0400

    breaking styles, fixing styles

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const music_list = [
     music: "audio/Mos Def - I'm Leaving.mp3"
   },
   {
-    img: 'img/sanPedro.jpg',
+    img: 'img/mogwai.jpg',
     name: 'San Pedro',
     artist: 'Mogwai',
     music: "audio/Mogwai - San Pedro.mp3"
@@ -106,28 +106,7 @@ function loadTrack(track_index) {
   updateTimer = setInterval(setUpdate, 1000);
 
   current_track.addEventListener('ended', nextTrack);
-  // random_bg_color();
 }
-
-// function random_bg_color() {
-//   let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
-//   let a;
-
-//   function populate(a){
-//     for(let i = 0; i < 6; i++){
-//       let x = Math.round(Math.random() * 14);
-//       let y = hex[x];
-//       a += y;
-//     }
-//     return a;
-//   }
-//   let Color1 = populate('#');
-//   let Color2 = populate('#');
-//   var angle = 'to right';
-
-//   let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
-//   document.body.style.background = gradient;
-// }
 
 function reset() {
   current_time.textContent = "00:00";
